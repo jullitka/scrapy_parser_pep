66014d255207a2ace29e20082f63ce6f86acb301
jullitka <julia_vadimovna_@mail.ru>
2023-07-04 14:54:29 +0300

    changed

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,6 +1,6 @@
-
 import csv
 from datetime import datetime
+from collections import defaultdict
 from pathlib import Path
 
 
@@ -10,9 +10,7 @@ BASE_DIR = Path(__file__).parent.parent
 class PepParsePipeline:
 
     def open_spider(self, spider):
-        self.count_statuses = {}
-        self.FILE_DIR = BASE_DIR / 'results'
-        self.FILE_DIR.mkdir(exist_ok=True)
+        self.count_statuses = defaultdict(int)
 
     def process_item(self, item, spider):
         status = item['status']
@@ -20,12 +18,14 @@ class PepParsePipeline:
         return item
 
     def close_spider(self, spider):
+        FILE_DIR = BASE_DIR / 'results'
+        FILE_DIR.mkdir(exist_ok=True)
         time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-        filepath = self.FILE_DIR / f'status_summary_{time}.csv'
+        filepath = FILE_DIR / f'status_summary_{time}.csv'
         with open(filepath, 'w', encoding='utf-8') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerows((
-                ('Статус', 'Количество'),
+                ('Status', 'Quantity'),
                 *self.count_statuses.items(),
                 ('Total', sum(self.count_statuses.values()))
             ))
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -13,19 +13,16 @@ class PepSpider(scrapy.Spider):
             'section[id^="numerical-index"] table a::attr(href)'
         ).getall()[::2]
         for pep_link in pep_links:
-            yield response.follow(pep_link, callback=self.parse_pep)
+            yield response.follow(f'{pep_link}/', callback=self.parse_pep)
 
     def parse_pep(self, response):
         number_name = response.css('h1.page-title::text').get().split(' – ')
-        number = int(
-            number_name[0].replace('PEP ', '')
-        )
+        number = int(number_name[0].replace('PEP ', ''))
+        status = response.css('dt:contains("Status") + dd abbr::text').get()
         name = number_name[1]
         data = {
             'number': number,
             'name': name,
-            'status': response.css(
-                'dt:contains("Status") + dd abbr::text'
-            ).get()
+            'status': status
         }
         yield PepParseItem(data)
